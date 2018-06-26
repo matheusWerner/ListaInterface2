088915d2f3744254cd4f7a48e9e75d0ff5a05259
matheus ruan werner <matheus.lol45@gmail.com>
2018-06-26 09:18:31 -0300

    Initial commit

---
+++ src/Exercicio01/Ex01.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercicio01;
+
+/**
+ *
+ * @author Alunos
+ */
+public class Ex01 {
+    
+    int numero = 0;
+    
+}
